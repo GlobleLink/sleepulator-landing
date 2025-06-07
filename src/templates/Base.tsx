e2ec1e269975ad6512af72621ee86db3c36aa48d
GlobleLink <globleiq@gmail.com>
2025-06-07 15:55:59 +1200

    Update Base.tsx

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,9 +1,9 @@
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
-import { Banner } from './Banner';
-import { Footer } from './Footer';
-import { Hero } from './Hero';
-import { Sponsors } from './Sponsors';
+import { Banner } from '../Banner';
+import { Footer } from '../Footer';
+import { Hero } from '../hero/Hero';
+import { Sponsors } from '../Sponsors';
 import { VerticalFeatures } from './VerticalFeatures';
 
 const Base = () => (
