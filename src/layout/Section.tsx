737cc996199c7047f0253bac6ad2d5c743831693
GlobleLink <globleiq@gmail.com>
2025-06-07 16:39:11 +1200

    Update Section.tsx

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -4,14 +4,13 @@ type ISectionProps = {
   title?: string;
   description?: string;
   yPadding?: string;
+  className?: string; // ✅ 新增 className 支持
   children: ReactNode;
 };
 
 const Section = (props: ISectionProps) => (
   <div
-    className={`mx-auto max-w-screen-lg px-3 ${
-      props.yPadding ? props.yPadding : 'py-16'
-    }`}
+    className={`mx-auto max-w-screen-lg px-3 ${props.yPadding ?? 'py-16'} ${props.className ?? ''}`}
   >
     {(props.title || props.description) && (
       <div className="mb-12 text-center">
@@ -29,3 +28,4 @@ const Section = (props: ISectionProps) => (
 );
 
 export { Section };
+
